7db992b1f7b76b7c37daff7d028e361669415617
sammwas <samson.m.njuguna@gmail.com>
2017-06-16 17:54:35 +0300

    first commit

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+//business logic
+var total1 = 0;
+var total2 = 0;
+var player1 = "tom";
+var player2 = "hardy";
+var agg1 = 0;
+var agg2 = 0;
+var subtotal=0;
+var winPoints=100;
+
+
+for(var x=0;x<5;x++)
+paint = Math.floor(Math.random() * 6 + 1);
+  {
+   // total2=0;
+  if (paint === 1) {
+    total2 = 0;
+    // alert("You are up player 2");
+    subtotal=total2;
+
+  } else {
+    // alert(total2 += paint);
+    subtotal+=total2;
+    agg1+=subtotal
+  }
+};
+
+// console.log(agg1);
