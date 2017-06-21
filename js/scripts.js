16a116e0a7c8dfdd6f932f5775013a829ac63463
sammwas <samson.m.njuguna@gmail.com>
2017-06-21 08:12:44 +0300

    the buttons now have functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,103 +1,73 @@
-//business logic
-// var total1 = 0;
-// var total2 = 0;
-// var player1 = "tom";
-// var player2 = "hardy";
-// var agg1 = 0;
-// var agg2 = 0;
-// var subtotal=0;
-// var winPoints=100;
-//
-//
-// for(var x=0;x<5;x++)
-// paint = Math.floor(Math.random() * 6 + 1);
-//   {
-//    // total2=0;
-//   if (paint === 1) {
-//     total2 = 0;
-//     // alert("You are up player 2");
-//     subtotal=total2;
-//
-//   } else {
-//     // alert(total2 += paint);
-//     subtotal+=total2;
-//     agg1+=subtotal
-//   }
-// };
+// user interface
+var Player=function(numbers,scores){
 
-// console.log(agg1);
-function Player(name,points) {
-    this.name = name;
-    this.points = points;
+  this.numbers=numbers;
+  this.scores=scores;
 }
 
-var player1 = new Player();
-var player2 = new Player();
+var score=0;
+var score2=0;
 
-var score1 = 0;
-var score2 = 0;
+// business logic
+$(document).ready(function(){
 
-Player.prototype.checkWin1 = function () {
-    var diceRoll1 = Math.floor(Math.random() * 6 + 1);
+  $("button#rollbutton").click(function(){
+    var number =Math.floor((Math.random() * 6) + 1);
 
-    if (diceRoll1 == 1) {
-        alert('player 2 you are up!');
-        score1 = 0;
-    } else {
-        if (score1 >= 100) {
-            alert('game over, Player 1 wins')
-        } else {
-            console.log('game continues');
-            player1.points = diceRoll1;
-            score1 += diceRoll1;
-            console.log(score1);
-            console.log('You rolled a die of: ' + diceRoll1 + ' for a score of: ' + score1);
-        }
+    if(number>1){
+       score +=number;
+       if(score>=100){
+         alert("player1 won");
+         $("button").prop('disabled',true);
+       }
     }
-}
-
-
-
-Player.prototype.checkWin2 = function () {
-    var diceRoll2 = Math.floor(Math.random() * 6 + 1);
-
-    if (diceRoll2 === 1) {
-        alert('its Players 1 turn');
-        score2 = 0;
-    } else {
-        if (diceRoll2 >= 100) {
-            alert('player 2 wins the game');
-        } else {
-            console.log('game continues');
-            player2.points = diceRoll2;
-            score2 += diceRoll2;
-            console.log(score2);
-            console.log('You rolled a die of: ' + diceRoll2 + ' for a score of: ' + score2);
-        }
+    else{
+       score=0;
+      alert("PLAYER 2 START");
+      $("button#rollbutton2").show();
+      $("button#rollbutton").hide();
     }
 
+    /*instance of player object*/
+var player1= new Player(number,score);
+    $("ul#finalresults").text("you rolled"+" " +player1.numbers);
+    $("ul#score").text("your current score is"+" " +player1.scores);
 
-    console.log(player1);
-    console.log(player2);
+  })
+  /*click function for player2 roll*/
 
+  $("button#rollbutton2").click(function(){
+    var number2 =Math.floor((Math.random() * 6) + 1);
 
-    //user interface.
-    $(document).ready(function () {
-        $('form#player1').click(function () {
-            // event.preventDefault();
-            // var name1Display = ($('input#game1').val());
-            // console.log(name1Display);
-            // $('#player1NameDisplay').text(name1Display);
-            $('#score1').text(score1);
-            $('#roll1').player1.checkWin1();
-        });
-        $('form#player2').click(function () {
-            // event.preventDefault();
-            // var name2Display = ($('input#game2').val());
-            // console.log(name2Display);
-            // $('#player2NameDisplay').text(name2Display);
-            $('#score2').text(score2);
-            $('#roll2').player2.checkWin2();
-        });
+    if(number2>1){
+       score2 +=number2;
+       if(score2>=100){
+         alert("player1 won");
+         $("button").prop('disabled',true);
+       }
+    }
+    else{
+       score2=0;
+      alert("PLAYER 1 START");
+      $("button#rollbutton").show();
+      $("button#rollbutton2").hide();
 
-    });
+    }
+    /*instance of player object*/
+var player2= new Player(number2,score2);
+    $("ul#finalresults2").text("you rolled"+" " +player2.numbers);
+    $("ul#score2").text("your current score is"+" " +player2.scores);
+
+  })
+  $("button#holdbutton").click(function(){
+    $("button#rollbutton2").show();
+    $("button#rollbutton").hide();
+
+  })
+  $("button#holdbutton2").click(function(){
+    $("button#rollbutton").show();
+    $("button#rollbutton2").hide();
+
+  })
+
+});
